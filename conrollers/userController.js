d5167257d7d3d018e533569bf640674c70b542c6
unknown <sabri.rakrouki@esprit.tn>
2022-07-20 15:33:34 +0200

    finish auth and register for user

--- conrollers/userController.js
+++ conrollers/userController.js
@@ -10,16 +10,18 @@ const userController = {
         try {
             const result = simpleUserSchema.validateAsync(req.body);
             console.log(result)
-            const { name, email, password, surname, birthDate, phoneNumber } = req.body;
+            const { username,name, email, password, surname, birthDate, phoneNumber } = req.body;
             const user = await User.findOne(email)
 
             if (user) return res.status(400).json({ msg: 'the email already exists.' })
             if (password.length < 8)
                 return res.status(400).json({ msg: "password is at least 8 char length" })
             const passwordHash = await bcrypt.hash(password, 10)
+           
             const newUser = new Users({
-                name, email, password: passwordHash
+                name, email, password: passwordHash,surname, birthDate, phoneNumber,username,role
             })
+            newUser.role="simpleUser"
             //Save mongodb
             await newUser.save();
             const accesstokent = createAccessToken({ id: user._id, role: user.role })
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const userController = require('../conrollers/userController');
-const roleMiddelWare=require('../middelWare/roleMiddelWare')
+const { auth, authAdmin } = require('../middelWare/authentification')
+const roleMiddelWare = require('../middelWare/roleMiddelWare')
 
 
-router.post('/register',userController.adminRegister)
-router.post('/login',userController.login)
-router.get('/logout',userController.logout)
-router.get('/refresh_token',userController.refreshToken)
+router.post('/register', userController.adminRegister)
+router.post('/login', userController.login)
+router.get('/logout', userController.logout)
+router.get('/refresh_token', auth, roleMiddelWare("admin"),userController.refreshToken)
 
 module.exports = router;
